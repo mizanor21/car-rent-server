a5d9e203b75c9589117ed0e1b6615658a4abd2d3
mizanor21 <mizanorrahman398@gmail.com>
2023-02-27 13:05:51 +0600

    car info part added

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Simple Node Server Running');
 })
 
-// DB_USER: carRent
-// DB_PASS: nuMSbh03vsiOvBBQ
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://carRent:nuMSbh03vsiOvBBQ@cluster0.disah5t.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-app.use(cors());
-const users = [
-    { id: 1, name: 'Shabana', email: 'shabana@gmail.com' },
-    { id: 2, name: 'Shanoor', email: 'Shanoor@gmail.com' },
-    { id: 3, name: 'Sabila', email: 'Sabila@gmail.com' },
+async function run() {
+    try {
+        const carCollection = client.db('carRent').collection('carInfo')
 
-];
+        app.get('/car-info', async (req, res) => {
+            const query = {};
+            const cursor = carCollection.find(query);
+            const carInfo = await cursor.toArray();
+            res.send(carInfo);
+        })
+    }
+    finally {
 
-app.get('/users', (req, res) => {
-    res.send(users);
-})
+    }
+
+}
+run().catch(console.dir)
 
 app.listen(port, () => {
     console.log(`Simple node server running on port ${port}`);
